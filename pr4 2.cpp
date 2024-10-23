d058f67123a4bfc0a28945d54e723f67541f66cc
karynakostik <karyna.kostik@gmail.com>
2024-10-23 15:16:03 +0300

    Removed testing comment

--- pr4 2.cpp
+++ pr4 2.cpp
@@ -12,4 +12,4 @@ int main()
 	cout << "\n¬вед≥ть x: ";
 	cin >> x;
 	cout << "\n„и Ї х парним двозначним числом? " << boolalpha << (x >= 10 && x <= 99 && x % 2 == 0) << endl;
-} //testing comment
+}
