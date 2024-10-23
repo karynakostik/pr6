8021dd6754a6deb9b90f1e225543fefd84276e66
karynakostik <karyna.kostik@gmail.com>
2024-10-23 14:52:34 +0300

    Add files via upload

---
+++ pr4 2.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include "windows.h"
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int x;
+	cout << "\n¬вед≥ть x: ";
+	cin >> x;
+	cout << "\n„и Ї х парним двозначним числом? " << boolalpha << (x >= 10 && x <= 99 && x % 2 == 0) << endl;
+}
